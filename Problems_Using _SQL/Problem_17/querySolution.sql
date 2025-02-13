69ac98e4f4be9415e3fca9f3590764a54c575202
Abdul Baset Saleh Shaiea Saleh <155334992+asshaieas@users.noreply.github.com>
2025-02-13 11:32:54 +0300

    Delete Problems_Using _SQL directory

--- Problems_Using _SQL/Problem_17/querySolution.sql
+++
@@ -1,6 +0,0 @@
--- Calculate the total population of all cities in Asia
-
-SELECT SUM(city.Population)  -- Summing the population of cities
-FROM city
-INNER JOIN country ON city.CountryCode = country.Code  -- Joining city and country tables using the country code
-WHERE country.Continent = 'Asia';  -- Filtering only for countries in the Asian continent
