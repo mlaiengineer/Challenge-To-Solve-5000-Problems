e65e6e02ef0e1a26307aa9b61d9826b27dcdb433
Abdul Baset Saleh Shaiea Saleh <155334992+asshaieas@users.noreply.github.com>
2025-02-16 20:53:51 +0300

    Delete Problem 15 directory

--- Problem 15/TestCase.java
+++
@@ -1,6 +0,0 @@
-public class TestCase {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.reverseExponentiation(4));
-    }
-}
